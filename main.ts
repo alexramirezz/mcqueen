abcf1cf03e55c5476955d73b61f24fbdb7f5a2cb
alexramirezz <106143386+alexramirezz@users.noreply.github.com>
2022-05-31 10:14:20 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 let Izq = 0
 let Der = 0
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     Izq = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     Der = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
-    if (!(Izq)) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    if (!Der) {
+        maqueen.motorRun(100, maqueen.Dir.CW, 30)
     }
-    if (!(Der)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+    
+    if (!Izq) {
+        maqueen.motorRun(100, maqueen.Dir.CW, 30)
     }
+    
 })
